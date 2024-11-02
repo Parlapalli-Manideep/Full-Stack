f7dbac5dd31b6f60d07e8201697a3d7b966ac76c
Manideep Parlapalli <manideepparlapalli@gmail.com>
2024-11-02 22:07:30 +0530

    array Practice added

---
+++ JS/Array/Array.js
@@ -0,0 +1,76 @@
+var a = [1,2,3,4,5,6]
+console.log(a.length)
+
+var a = [1,2,3,4,5,6]
+a.push(7)
+console.log(a)
+
+var a = [1,2,3,4,5,6]
+a.pop()
+console.log(a)
+
+var a = [1,2,3,4,5,6]
+a.unshift(0)
+console.log(a)
+
+var a = [1,2,3,4,5,6]
+a.shift(0)
+console.log(a)
+
+var a = [1,2,3,4,5,6]
+a.reverse()
+console.log(a)
+
+var a = [1,2,3,4,5,6]
+console.log(a[0])
+
+var a = [1,2,3,4,5,6]
+console.log(a.includes(5))
+
+a = [4,5,6,1,2,3]
+a.sort()
+console.log(a)
+
+var a = [1,2,3,4,5,6]
+a =a.join(",")
+console.log(a)
+
+
+var a = [1,2,3,4,5,6]
+a.push(7)
+console.log(a)
+a.shift()
+console.log(a)
+
+var a = [1,2,3,4,5,6]
+a.reverse()
+console.log(a)
+a = a.join("")
+console.log(a)
+
+a = [4,5,6,1,2,3]
+a.sort()
+console.log(a)
+a.shift()
+console.log(a)
+
+var a = [1,2,3,4,5,6]
+a.unshift(0)
+a.unshift(100)
+console.log(a)
+a.shift(0)
+console.log(a)
+console.log(a.length)
+
+var a = [3,2,1,0]
+var b =[7,8,9]
+var c = a.concat(b)
+console.log(c)
+c.sort()
+console.log(c)
+c.pop()
+c.shift()
+console.log(c)
+
+
+
