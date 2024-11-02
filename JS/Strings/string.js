8a968da1d2ee4d5a4ec641a2effb4522aef702b2
Manideep Parlapalli <manideepparlapalli@gmail.com>
2024-11-02 15:20:06 +0530

    js variables added

---
+++ JS/Strings/string.js
@@ -0,0 +1,17 @@
+var a= ["one ","two","three" ]
+console.log(a.length)
+console.log(a)
+a.push(7 , 'jklsdf') //end of the array
+console.log(a.length)
+console.log(a)
+console.log(a[2].slice(-2))
+var kkk = a.reverse()
+console.log(a)
+console.log(kkk)
+console.log(a.includes("one "))
+var a = "hello world"
+console.log(a.split())
+console.log(a.split(""))
+console.log(a.split(" "))
+console.log(a.split("o"))
+console.log(a.split("z"))
