7f17991a870fd46e1fac34912a11f1929e2d2f1f
Manideep Parlapalli <manideepparlapalli@gmail.com>
2024-11-03 00:25:12 +0530

    switch practice added

---
+++ JS/Switch/switch.js
@@ -0,0 +1,50 @@
+var day = "tuesday"
+switch (day)
+{
+    case "monday":
+        {
+        console.log("today is " + day)
+        break
+    }
+    case "tuesday":
+        {
+        console.log("today is " + day)
+        break
+    }
+    case "wednesday":
+        {
+        console.log("today is " + day)
+        break
+    }
+    case "thursday":
+        {
+        console.log("today is " + day)
+        break
+    }
+    case "friday":
+        {
+        console.log("today is " + day)
+        break
+    }
+    case "saturday":
+        {
+        console.log("today is " + day)
+        break
+    }
+    case "sunday":6
+        {
+        console.log("today is " + day)
+        break
+    }
+}
+
+
+var a =[1,2,3,4,5]
+for(var i of a)
+    console.log(i)
+
+a = "manideep"
+a = a.split("")
+for(i of a)
+    console.log(i)
+    
