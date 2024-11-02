e46f4e9c3a4428ac8046d84974a1ce5cb3f28ebf
Manideep Parlapalli <manideepparlapalli@gmail.com>
2024-11-02 23:31:00 +0530

    if statements practice added

---
+++ JS/If_statements/if.js
@@ -0,0 +1,27 @@
+var age = 22
+if (age > 18)
+    console.log("Eligible for vote")
+else
+    console.log("Not eligible to vote")
+
+weather = "rainy"
+if (weather =="rainy")
+    console.log("Its pouring outside")
+else
+    console.log("Not raining")
+
+marks = 76
+if (marks > 90)
+    console.log("A+")
+else if(marks >= 81 && marks <=90)
+    console.log("A")
+else if(marks >=71 && marks <=80)
+    console.log("B+")
+else if(marks >= 61 && marks <=70)
+    console.log("C+")
+else if(marks >= 51 && marks <=60)
+    console.log("C")
+else if(marks >= 40 && marks <=50)
+    console.log("C")
+else
+    console.log("fail")
