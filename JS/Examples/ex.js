457a16e89fcf527ec7fdc15f40935c7f7bf4688b
Manideep Parlapalli <manideepparlapalli@gmail.com>
2024-11-03 01:27:03 +0530

    practice on if else

---
+++ JS/Examples/ex.js
@@ -0,0 +1,39 @@
+const score = 85 
+const grade = (score >= 90) ? 'A' :
+              (score >= 80) ? 'B' :
+              (score >= 70) ? 'C' :
+              (score >= 60) ? 'D' : 'F';
+console.log(`Your grade is: ${grade}`);
+
+
+const age = 48;
+const isAdult = (age >= 18) ? 'You are an adult.' : 'You are not an adult.';
+console.log(isAdult);
+
+var day ="sunday"
+if(day === "sunday" || day ==="saturday")
+    if(day === "sunday" )
+        console.log("today is sunday , it's a weekend")
+    else
+    console.log("today is saturday , it's a weekend")
+else
+console.log("It's not a weekend")
+
+var agee = 50
+citizenship = true
+if( agee >= 18 && citizenship === true)
+    console.log("eligible to vote")
+else
+    console.log("not eligible to vote")
+
+
+var a = "Admin"
+var b = a.toLowerCase(a)
+var c = a.toUpperCase(a)
+var password = 123456
+var userName = "ADMIN";
+pass = 12356
+if(( userName === a || userName === b || userName === c ) && pass ===123456)
+console.log("Login successfull")
+else
+console.log("check your credentials")
