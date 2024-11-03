9042d7e5043df48a21f0c66db9a0ee2f84c8f049
Manideep Parlapalli <manideepparlapalli@gmail.com>
2024-11-03 21:49:41 +0530

    strings practice added

---
+++ JS/Strings/prac.js
@@ -0,0 +1,23 @@
+var str = "bittu"
+str= str.toUpperCase()
+console.log(str)
+
+var s = "anirudh"
+var s3 = s.slice(1,4)
+console.log(s3)
+
+var arr = [1,2,3]
+arr.length = 0 
+arr.push("element")
+console.log(arr.length)
+
+var ss = "man"
+ss = ss.split("")
+ss.push("i")
+console.log( typeof ss)
+
+var aa = ["m", "a" , "n"]
+aa[10] = "z"
+resultedArray = aa.slice(8,11)
+console.log(resultedArray)
+console.log(resultedArray.length)
