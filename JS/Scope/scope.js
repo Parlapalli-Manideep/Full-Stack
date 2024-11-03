c5e7dc5a73c0dbd6a4f01b3eb0e54d95b6bb5acd
Manideep Parlapalli <manideepparlapalli@gmail.com>
2024-11-03 12:02:43 +0530

    scopes added

---
+++ JS/Scope/scope.js
@@ -0,0 +1,35 @@
+var a = 10 
+let b = 20 
+const c = 30
+if (true)
+{
+    var d = 10
+    let e = 5
+    const f = 465
+    console.log(d + e + f)
+}
+function one()
+{
+    var aa = 12
+    let bb = 56
+    const cc = 65
+    if( true)
+    {
+        var dd = 546
+        let ee = 89
+        const ff = 98
+        console.log(dd + ee + ff)
+    }
+}
+one()
+for( let i = 1 ; i <= 5 ;i++)
+    console.log(i)
+
+if (true)
+{
+    let i = 45
+    i +=55
+    console.log(i)
+}
+
+
